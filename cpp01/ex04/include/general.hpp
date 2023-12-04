c16138318a484f09c6f67e63e7a4960d781cc4d6
Martin <martinhaan91+git@gmail.com>
2023-12-04 16:31:45 +0100

    ex04 almost done

--- cpp01/ex04/include/general.hpp
+++ cpp01/ex04/include/general.hpp
@@ -4,4 +4,7 @@
 #include <fstream>
 #include <iostream>
 
+void find_replace(std::string &line, const std::string s1,
+                  const std::string s2);
+
 #endif
--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -2,9 +2,36 @@
 
 int main(int argc, char **argv) {
   if (argc != 4)
-    return (1);
-  std::fstream file;
-  file.open(argv[1], ios::in);
-  if (!file)
-    return (std::cout << "Error: unable to open file!" << std::endl, 1);
+    return (std::cout << "Progam takes 3 arguments." << std::endl, 1);
+
+  std::ifstream infile(argv[1]);
+  if (!infile)
+    return (std::cout << "Error: unable to open infile!" << std::endl, 1);
+
+  std::ofstream outfile(argv[1] + std::string(".replace"));
+  if (!outfile)
+    return (std::cout << "Error: unaable to create outfile!" << std::endl, 1);
+
+  const std::string s1 = argv[2];
+  const std::string s2 = argv[3];
+
+  std::string line;
+  while (getline(infile, line)) {
+    find_replace(line, s1, s2);
+    outfile << line << std::endl;
+  }
+
+  infile.close();
+  outfile.close();
+}
+
+void find_replace(std::string &line, const std::string s1,
+                  const std::string s2) {
+  size_t pos = 0;
+
+  while ((pos = line.find(s1, pos)) != std::string::npos) {
+    line.erase(pos, s1.length());
+    line.insert(pos, s2);
+    pos += s2.length();
+  }
 }
