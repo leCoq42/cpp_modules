9b1dac59b488d3f6a02fc9142dc42c15267f09d4
Martin Haan <mhaan@f1r5s10.codam.nl>
2023-12-08 17:16:07 +0100

    starting ex06

---
+++ cpp01/ex06/include/Harl.hpp
@@ -0,0 +1,14 @@
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+
+class Harl {
+public:
+  Harl();
+  ~Harl();
+
+private:
+};
+
+#endif
---
+++ cpp01/ex06/src/main.cpp
@@ -0,0 +1,3 @@
+#include "Harl.hpp"
+
+int main(unsigned int argc, char **argv) { return 0; }
