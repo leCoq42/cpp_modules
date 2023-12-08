d061d807fe852ccd382d56bd77a0df65c2984a9a
Martin <martinhaan91+git@gmail.com>
2023-12-08 11:39:39 +0100

    ex04 working

--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -2,15 +2,18 @@
 
 int main(int argc, char **argv) {
   if (argc != 4)
-    return (std::cout << "Progam takes 3 arguments." << std::endl, 1);
+    return (std::cerr << "Invalid Input: Progam takes 3 arguments."
+                      << std::endl,
+            1);
 
   std::ifstream infile(argv[1]);
   if (!infile)
-    return (std::cout << "Error: unable to open infile!" << std::endl, 1);
+    return (std::cerr << "Error: unable to open infile!" << std::endl, 1);
 
   std::ofstream outfile(argv[1] + std::string(".replace"));
   if (!outfile)
-    return (std::cout << "Error: unaable to create outfile!" << std::endl, 1);
+    return (infile.close(),
+            std::cerr << "Error: unable to create outfile!" << std::endl, 1);
 
   const std::string s1 = argv[2];
   const std::string s2 = argv[3];
